7baee27f6c8f67da8d546f70e842ed4af643c8c8
Markito <mafeorellana09@gmail.com>
2024-07-05 18:51:37 -0400

    actualizando el frontend, imagen de fondo, arreglo en routes y links, redireccionamiento de rutas

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 import UserService from '../service/UserService';
 import '../../styles/custom.css';
 
+// Importa tu imagen de logo
+import logo from '../../images/logo.png';
+
 function Navbar() {
     const isAuthenticated = UserService.isAuthenticated();
     const isAdmin = UserService.isAdmin();
-
-
 
     const handleLogout = () => {
         const confirmDelete = window.confirm('¿Estás seguro de que quieres cerrar sesión este usuario?');
@@ -16,45 +17,42 @@ function Navbar() {
         }
     };
 
-
     return (
-       
-
-<nav className="navbar navbar-expand-lg navbar-light bg-black">
-<div className="container-fluid">
-    <a className="navbar-brand nav-link" href="/">Mi Aplicación</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-            {!isAuthenticated && (
-                <li className="nav-item">
-                    <Link className="nav-link" to="/login">Marco Dev</Link>
-                </li>
-            )}
-            {isAuthenticated && (
-                <li className="nav-item">
-                    <Link className="nav-link" to="/profile">Perfil</Link>
-                </li>
-            )}
-            {isAdmin && (
-                <li className="nav-item">
-                    <Link className="nav-link" to="/admin/user-management">Gestión de usuarios</Link>
-                </li>
-            )}
-            {isAuthenticated && (
-                <li className="nav-item">
-                    <Link className="nav-link" to="/" onClick={handleLogout}>Cerrar sesión</Link>
-                </li>
-            )}
-        </ul>
-    </div>
-</div>
-</nav>
-
-
-
+        <nav className="navbar navbar-expand-lg navbar-light bg-black">
+            <div className="container-fluid">
+                {/* Logo a la izquierda */}
+                <a className="navbar-brand nav-link" href="/">
+                    <img src={logo} alt="Logo" style={{ width: '100px', borderRadius: '50%' }} /></a> 
+                <a className="navbar-brand nav-link" href="/">Inicio</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        {!isAuthenticated && (
+                            <li className="nav-item">
+                           <button id="btn-uno" className="btn btn btn-outline-danger custom-button"> <Link className="nav-link" to="/login">Login</Link> </button>
+                            </li>
+                        )}
+                        {isAuthenticated && (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/profile">Perfil</Link>
+                            </li>
+                        )}
+                        {isAdmin && (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/admin/user-management">Gestión de usuarios</Link>
+                            </li>
+                        )}
+                        {isAuthenticated && (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/" onClick={handleLogout}>Cerrar sesión</Link>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
--- frontend/src/components/userspage/IndexPage.jsx
+++ frontend/src/components/userspage/IndexPage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../images/estrellas.jpg';
 
 const IndexPage = () => {
     return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col">
-                    <h1>Bienvenido a nuestro Inicio</h1>
-                    <p>Esta es una plantilla básica para una nueva página en React.</p>
-                    <Link to="/login" className="btn btn-primary">¿Te gustaria iniciar Sesion?</Link>
+        <div className="background-image">
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col">
+                        <h1 className='text-white'>Bienvenidos a nuestro Inicio</h1>
+                        <p className='text-white'>Esta es una plantilla básica para una nueva página en React.</p>
+                        <Link to="/login" className="btn btn-primary">¿Te gustaria iniciar Sesion?</Link>
+                    </div>
                 </div>
             </div>
         </div>
