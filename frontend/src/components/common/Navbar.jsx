843de6f64fa1aec82378e855b5664f51314bb8d3
Markito <mafeorellana09@gmail.com>
2024-07-05 14:21:27 -0400

    cambios en el index de frontend react

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import UserService from '../service/UserService';
+import '../../styles/custom.css';
 
 function Navbar() {
     const isAuthenticated = UserService.isAuthenticated();
@@ -17,14 +18,43 @@ function Navbar() {
 
 
     return (
-        <nav>
-            <ul>
-                {!isAuthenticated && <li><Link to="/">Marco Dev</Link></li>}
-                {isAuthenticated && <li><Link to="/profile">Perfil</Link></li>}
-                {isAdmin && <li><Link to="/admin/user-management">Gestión de usuarios</Link></li>}
-                {isAuthenticated && <li><Link to="/" onClick={handleLogout}>Cerrar sesión</Link></li>}
-            </ul>
-        </nav>
+       
+
+<nav className="navbar navbar-expand-lg navbar-light bg-black">
+<div className="container-fluid">
+    <a className="navbar-brand nav-link" href="/">Mi Aplicación</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+            {!isAuthenticated && (
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Marco Dev</Link>
+                </li>
+            )}
+            {isAuthenticated && (
+                <li className="nav-item">
+                    <Link className="nav-link" to="/profile">Perfil</Link>
+                </li>
+            )}
+            {isAdmin && (
+                <li className="nav-item">
+                    <Link className="nav-link" to="/admin/user-management">Gestión de usuarios</Link>
+                </li>
+            )}
+            {isAuthenticated && (
+                <li className="nav-item">
+                    <Link className="nav-link" to="/" onClick={handleLogout}>Cerrar sesión</Link>
+                </li>
+            )}
+        </ul>
+    </div>
+</div>
+</nav>
+
+
+
     );
 }
 
