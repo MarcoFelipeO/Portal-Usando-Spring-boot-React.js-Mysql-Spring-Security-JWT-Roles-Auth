23cca1fc728d37ef030581b28dd4610065e6ff09
Marco Orellana <mafeorellana09@gmail.com>
2024-07-22 20:47:06 -0400

    cambios en pageindex2

--- frontend/src/components/userspage/PaginaNumeroDos.jsx
+++ frontend/src/components/userspage/PaginaNumeroDos.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../images/foto3.jpg';
+import '../../styles/custom.css';
+
 
 const PaginaNumeroDos = () => {
     return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col">
-                    <h1>Bienvenido a la Nueva Página</h1>
-                    <p>Esta es una plantilla básica para una nueva página en React.</p>
-                    <Link to="/" className="btn btn-primary">Volver a la Página Principal haz click aqui</Link>
+        <div className="background-image">
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col">
+                        <h1 className='text-white'>Bienvenido a la Nueva Página</h1>
+                        <p className='text-white'>Esta es una plantilla básica para una nueva página en React.</p>
+                        <Link to="/" className="btn btn-danger">Volver a la Página Principal haz click aqui</Link>
+                    </div>
                 </div>
             </div>
         </div>
